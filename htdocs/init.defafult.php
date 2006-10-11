180f0558f98cf0def053169fd35d63d4db94b22d
epithet <epithet@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-10-11 16:47:31 +0000

    Init/config separation.
    git-svn-id: http://svn.habariproject.org/habari/trunk@10 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/init.defafult.php
+++ htdocs/init.defafult.php
@@ -1,15 +1,36 @@
 <?php
 /**
- * Habari Initialization Class
- *
- * Requires PHP5.0.4 or later
- * @package Habari
- */
+ * Database connection samples
+ * 1. Copy this file to init.mine.php
+ * 2. Uncomment the section that applies to you
+ * 3. Change the connection string, username, and password, if required. 
+ * 3. ...
+ * 4. Profit!  
+ **/   
 
-function __autoload($class_name) {   
-    require_once 'system/classes/' . $class_name . '.php';
-}
 
-$connection_string = 'mysql:host=localhost;dbname=habari'; // shouldn't need to change this 
-$db = new habari_db( $connection_string, 'username', 'password' );
+/**
+ * MySQL Connection Details
+ ** 
+
+$db_connection = array(
+'connection_string' => 'mysql:host=habari.fearsome-engine.com;dbname=habari',  // MySQL Connection string
+'username' => 'mysql username',  // MySQL username
+'password' => 'mysql password',  // MySQL password
+);
+
+*/
+
+/**
+ * SQLite Connection Details
+ **
+
+$db_connection = array(
+'connection_string' => 'sqlite:' . dirname(__FILE__) . '/habari.db',  // SQLite Connecton string
+'username' => null,  // SQLite username
+'password' => null,  // SQLite password
+);
+
+*/
+
 ?>
