ceb5282abf143c55b4a5c68462e894799e4b1f75
epithet <epithet@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-10-17 19:43:49 +0000

    Get rid of init.php and Posts class.  Moved init.php to htdocs/index.php and posts.php to post.php
    git-svn-id: http://svn.habariproject.org/habari/trunk@37 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,14 +1,49 @@
 <?php
 
 define('HABARI_PATH', dirname(__FILE__));
-include_once( HABARI_PATH . '/system/init.php' );
+
+/**
+ * function __autoload
+ * Autoloads class files for undeclared classes.
+ **/  
+function __autoload($class_name) {
+	if(file_exists(HABARI_PATH . '/user/classes/' . strtolower($class_name) . '.php'))
+		require_once HABARI_PATH . '/user/classes/' . strtolower($class_name) . '.php';
+	else if(file_exists(HABARI_PATH . '/system/classes/' . strtolower($class_name) . '.php'))
+		require_once HABARI_PATH . '/system/classes/' . strtolower($class_name) . '.php';
+	else
+		die( 'Could not include class file ' . strtolower($class_name) . '.php' );
+}
+
+// Load the config
+if(file_exists(HABARI_PATH . '/config.php')) {
+	require_once HABARI_PATH . '/config.php';
+} else {
+	die('There are no database connection details.  Please rename config-sample.php to config.php and edit the settings therein.');	
+}
+
+// Connect to the database or fail informatively
+try {
+	$db = new habari_db( $db_connection['connection_string'], $db_connection['username'], $db_connection['password'] );
+}
+catch( Exception $e) {
+	die( 'Could not connect to database using the supplied credentials.  Please check config.php for the correct values. Further information follows: ' .  $e->getMessage() );		
+}
+unset($db_connection);
+
+// Create the global options instance
+$options = new Options();
+
+// Install the database tables if they're not already installed
+Installer::install();
+
 
 ?>
 <?php include_once(HABARI_PATH . '/themes/k2/header.php'); ?>
 <div class="content">
 	<div id="primary">
 		<div id="primarycontent" class="hfeed">
-			<?php foreach ( posts::retrieve() as $post ) { ?>
+			<?php foreach ( Post::get_posts() as $post ) { ?>
 				<div id="<?php echo $post->guid; ?>">
 					<div class="entry-head">
 						<h3 id="entry-title" class="entry-title"><a href="/<?php echo $post->slug; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title; ?></a></h3>
