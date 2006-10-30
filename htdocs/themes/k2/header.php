fecc7a4d392f38b66b9707c748853c45d763d8f1
chrisdmitri <chrisdmitri@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-10-30 20:29:34 +0000

    This is the CSS and HTML for the current admin area, as well as the addition of nickname to the installer and user class.  Themehandler is also updated with a reference to so-fresh (commented out) since I am currently porting it to Habari
    git-svn-id: http://svn.habariproject.org/habari/trunk@99 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/themes/k2/header.php
+++ htdocs/themes/k2/header.php
@@ -4,14 +4,14 @@
 	<title><?php Options::out('blog_title'); ?></title>
 	<meta http-equiv="Content-Type" content="text/html;" />
 	<meta name="generator" content="Habari" />
-	
   	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/rss2.php" />
-  	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="/atom.php" />
+  	<link rel="alternate" type="application/atom+xml" title="Atom 0.3" href="<?php echo $options->base_url; ?>atom" />
 	<link type="application/x.atom+xml" rel="service.post" href="<?php echo $urlparser->get_url('collection'); ?>" title="<?php Options::out('blog_title'); ?>"/>   	
 
 	<link rel="stylesheet" type="text/css" media="screen" href="themes/k2/style.css" />
 
 	<?php if ( User::identify() ) { // Still needs to check for edit permissions ?>
+		
 	<style type="text/css">
 		.eip_editable { background-color: #ff9; }
 		.eip_savebutton { background-color: #36f; color: #fff; }
