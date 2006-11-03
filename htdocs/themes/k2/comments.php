c06ddb30d526a821cd43d01df87fd5911a796db4
chrisdmitri <chrisdmitri@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-11-03 03:29:25 +0000

    Let's see here, fixing the single post feed in atom handler, and adding commenting functionality.  This is of course open for review, but my thinking is that we have a ContentHandler that handles interacting with all of our content: post and comment. Currently it only handles creating comments, but it could easily be updated to handle editing and updating of comments.
    Next would be to add post creation and editing functionality.  This is open for improvement, but at least we can actually
    create comments.
    
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk@123 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/themes/k2/comments.php
+++ htdocs/themes/k2/comments.php
@@ -6,7 +6,8 @@
 <div class="comments">
 	<h4><span id="comments"><?php echo $post->comment_count; ?> Responses to </span><?php echo $post->title; ?></h4>
 	<div class="metalinks">
-		<span class="commentsrsslink"><a href="/<?php echo $post->slug; ?>/feed">Feed for this Entry</a></span>
+		<span class="commentsrsslink"><a href="/<?php echo $post->slug; ?>/atom">Feed for this Entry</a></span>
+		<span class="trackbacklink"><a href="<?php echo $post->slug; ?>/trackback">Trackback Address</a></span>
 	</div>
 	<ol id="commentlist">
 		<?php foreach ( $post->comments as $comment ) { ?>
@@ -26,4 +27,30 @@
 			</li>
 		<?php } ?>
 	</ol>
+	<div class="comments">
+		<h4 id="respond" class="reply">Leave a Reply</h4>
+			<form action="<?php Options::out('base_url'); ?>admin/process/add_comment" method="post" id="commentform">
+			<input type="hidden" name="post_slug" value="<?php echo $post->slug; ?>" />
+				<div id="comment-personaldetails">
+					<p>
+						<input type="text" name="name" id="name" value="" size="22" tabindex="1" />
+						<label for="author"><small><strong>Name</strong></small></label>
+					</p>
+					<p>
+						<input type="text" name="email" id="email" value="" size="22" tabindex="2" />
+						<label for="email"><small><strong>Mail</strong> (will not be published) </small></label>
+					</p>
+					<p>
+						<input type="text" name="url" id="url" value="" size="22" tabindex="3" />
+						<label for="url"><small><strong>Website</strong></small></label>
+					</p>
+				</div>
+				<p><textarea name="content" id="content" cols="100%" rows="10" tabindex="4"></textarea></p>
+				<p>
+					<input name="submit" type="submit" id="submit" tabindex="5" value="Submit" />
+				</p>
+				<div class="clear"></div>
+			</form>
+	</div>
+	<hr />
 </div>
