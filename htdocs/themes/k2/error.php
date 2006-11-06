fdc7c6550034cffa76c5e3c78d465c9e39ac1996
smerrill <smerrill@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-11-06 02:09:06 +0000

    adding error.php which was absent from the previous commit
    git-svn-id: http://svn.habariproject.org/habari/trunk@141 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ htdocs/themes/k2/error.php
@@ -0,0 +1,30 @@
+<?php include_once(HABARI_PATH . '/themes/k2/header.php'); ?>
+<div class="content">
+	<div id="primary">
+		<div id="primarycontent" class="hfeed">
+				<div id="error">
+					<div class="entry-head">
+						<h3 id="entry-title" class="entry-title">Error!</h3>
+							<div id="entry-content" class="entry-content">
+								The requested post was not found.
+							</div>
+					</div>
+				</div>
+		</div>
+	</div>
+	<hr />
+<div class="secondary">
+	<div id="search"><h2>Search</h2>
+<form method="get" id="searchform" action="/index.php">
+	<input type="text" id="s" name="s" value="search blog archives" />
+	<input type="submit" id="searchsubmit" value="go" />
+</form>
+	</div>	
+	<div class="sb-about">
+		<h2>About</h2>
+				<p><?php Options::out('about'); ?></p>
+	</div>	
+</div>
+<div class="clear"></div>
+</div>
+<?php include_once('themes/k2/footer.php'); ?>
