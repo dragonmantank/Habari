2f8b7ac4131277ba85b1a75b870d47a4dbafc332
epithet <epithet@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2006-12-22 04:25:02 +0000

    You're going to want the spam plugin I mentioned in the last commit.  Otherwise, nothing works.  ;)
    git-svn-id: http://svn.habariproject.org/habari/trunk@191 653ae4dd-d31e-0410-96ef-6bf7bf53c507

---
+++ htdocs/user/plugins/spamchecker.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * SpamChecker Class
+ * 
+ * This class is a demonstration of a Habari plugin.
+ * It should probably not be distributed with the Habari source
+ * unless it is significantly improved.
+ **/     
+
+class SpamChecker extends Plugin
+{
+
+	/**
+	 * function filter_add_comment
+	 * This function is executed when the filter "add_comment" is applied to a Comment object.
+	 * The parent class, Plugin, handles registering the filter and hook name using the 
+	 * name of the function to determine where it will be applied.
+	 * You can still register functions as hooks without using this method, but boy, is it handy.
+	 * Note that other plugins may filter this data also, so the Comment object should pass
+	 * through this function relatively unscathed.  The implication of this, for example
+	 * is that you shouldn't return null from this function.	 	 
+	 * @param Comment The comment that will be processed before storing it in the database.
+	 * @return Comment The comment result to store.
+	 **/	 	 	 	 	 	 	 	
+	function filter_add_comment($comment)
+	{
+		// This plugin ignores non-comments
+		if($comment->type != Comment::COMMENT) {
+			return $comment;
+		}
+		
+		// Comments with more than one link are spam
+		// When we implement real spam protection, this will look even more like a joke. 
+		preg_match_all('/<\s*a\s.*?<\s*\/\s*a\s*>/', $comment->content, $matches);
+		if(count($matches[0]) > 1) {
+			$comment->status = Comment::STATUS_SPAM;
+		}
+		return $comment;
+	}
+
+}
+
+?>
