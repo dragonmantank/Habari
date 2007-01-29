94c5963d839836d247054a776e00e46f9d714df4
smerrill <smerrill@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2007-01-29 04:25:19 +0000

    Adding initial multi-site support, per ticket #158.
    The default site configuration file lives at {HABARI_PATH}/config.php
    
    Additional site configuration files live at 
    {HABARI_PATH}/user/sites/*/config.php
    
    The directories are searched in exactly the same way that Drupal does 
    it.  We need to write up meaningful documentation for this.
    
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk@455 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/index.php
+++ htdocs/index.php
@@ -22,48 +22,58 @@ ob_start();
  * Autoloads class files for undeclared classes.
  **/  
 function __autoload($class_name) {
-	if(file_exists(HABARI_PATH . '/user/classes/' . strtolower($class_name) . '.php'))
-		require_once HABARI_PATH . '/user/classes/' . strtolower($class_name) . '.php';
-	else if(file_exists(HABARI_PATH . '/system/classes/' . strtolower($class_name) . '.php'))
-		require_once HABARI_PATH . '/system/classes/' . strtolower($class_name) . '.php';
+	$class_file = strtolower($class_name) . '.php';
+	if(file_exists(HABARI_PATH . '/user/classes/' . $class_file))
+		require_once HABARI_PATH . '/user/classes/' . $class_file;
+	else if(file_exists(HABARI_PATH . '/system/classes/' . $class_file))
+		require_once HABARI_PATH . '/system/classes/' . $class_file;
 	else
-		die( 'Could not include class file ' . strtolower($class_name) . '.php' );
+		die( 'Could not include class file ' . $class_file );
 }
 error_reporting(E_ALL);
 // Undo what magic_quotes_gpc might have wrought
 Utils::revert_magic_quotes_gpc();
 
+// find and load the config.php file
+$config = Utils::get_config_dir() . '/config.php';
+
 // Load the config
-if(file_exists(HABARI_PATH . '/config.php')) {
-	require_once HABARI_PATH . '/config.php';
-  /*
-   * This is a quick hack to load the DB tables if someone
-   * already has a config.php set up, but no tables...
-   * 
-   * @todo  make an decent solution to this... involves an upgrade plan.
-   */
-  if (DB::connect()) {
-    $sql= "SELECT COUNT(*) FROM " . $db_connection['prefix'] . "posts";
-    if (! @ DB::query($sql)) {
-      $installer= new InstallHandler();
-      $installer->begin_install();
-    }
-  }
+if ( file_exists($config) ) {
+	require_once $config;
+	if ( ! isset($db_connection) )
+	{
+		$installer= new InstallHandler();
+		$installer->begin_install();
+	}
+	/*
+	 * This is a quick hack to load the DB tables if someone
+	 * already has a config.php set up, but no tables...
+	 * 
+	 * @todo  make an decent solution to this... involves an upgrade plan.
+	 */
+	if (DB::connect())
+	{
+		$sql= "SELECT COUNT(*) FROM " . $db_connection['prefix'] . "posts";
+		if (! @ DB::query($sql))
+		{
+			$installer= new InstallHandler();
+			$installer->begin_install();
+		}
+	}
 } 
-else {
-  /* 
-   * Fire up the InstallHandler to create the 
-   * configuration file in the proper directory
-   * and install the base database
-   */
-  $installer= new InstallHandler();
-  $installer->begin_install();
-  //exit;
+else
+{
+	/* 
+	 * Fire up the InstallHandler to create the 
+	 * configuration file in the proper directory
+	 * and install the base database
+	 */
+	$installer= new InstallHandler();
+	$installer->begin_install();
 }
 
 // Set the locale
 Locale::set();
-//Installer::install();
 
 // Activate all plugins - remove this when we have a plugin admin UI
 foreach(Plugins::list_all() as $plugin) {
@@ -76,19 +86,11 @@ foreach( Plugins::list_active() as $file ) {
 }
 Plugins::load();
 Plugins::act('plugins_loaded');
-/*$start_url= ( isset($_SERVER['REQUEST_URI']) 
-      ? $_SERVER['REQUEST_URI'] 
-      : $_SERVER['SCRIPT_NAME'] . 
-        ( isset($_SERVER['PATH_INFO']) 
-        ? $_SERVER['PATH_INFO'] 
-        : '') . 
-        ( (isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'] != '')) 
-          ? '?' . $_SERVER['QUERY_STRING'] 
-          : ''));
 
-$url= new URL($start_url);
-*/
+// parse and handle the request
 Controller::parse_request();
 Controller::dispatch_request();
+
+// flush the contents of output buffering
 ob_flush();
 ?>
