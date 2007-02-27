57300a9fc32959c712237a7cc10aab0cea78c104
moeffju <moeffju@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2007-02-27 02:57:09 +0000

    Some markup for i18n.
    git-svn-id: http://svn.habariproject.org/habari/trunk@545 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/user/themes/k2/footer.php
+++ htdocs/user/themes/k2/footer.php
@@ -2,9 +2,8 @@
 </div> <!-- Close Page -->
 <hr />
 <p id="footer"><small>
-<?php Options::out('title'); ?> is
-powered by <a href="http://www.habariproject.org/" title="Habari">Habari</a>
-and <a href="http://en.wikipedia.org/wiki/Earl_Grey_tea" title="Earl Grey tea" rel="nofollow">Earl Grey tea</a>. 
+<?php Options::out('title'); _e('is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a>
+<?php _e('and'); ?> <a href="http://en.wikipedia.org/wiki/Earl_Grey_tea" title="Earl Grey tea" rel="nofollow">Earl Grey tea</a>. 
 </small></p>
 
 <?php
