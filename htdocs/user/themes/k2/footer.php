f00afa26b123f664846e025fd4c14e53a83f4338
umbrae <umbrae@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2007-03-20 15:44:21 +0000

    Applying patch 280 for post info object, props to freakerz. Also a minor spacing fix for k2footer.
    git-svn-id: http://svn.habariproject.org/habari/trunk@579 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/user/themes/k2/footer.php
+++ htdocs/user/themes/k2/footer.php
@@ -2,7 +2,7 @@
 </div> <!-- Close Page -->
 <hr />
 <p id="footer"><small>
-<?php Options::out('title'); _e('is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a>
+<?php Options::out('title'); _e(' is powered by'); ?> <a href="http://www.habariproject.org/" title="Habari">Habari</a>
 <?php _e('and'); ?> <a href="http://i-want-a-pony.com/" title="A Pony" rel="nofollow">A Pony</a>. 
 </small></p>
 
