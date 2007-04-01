b10dafc7c736fec5c5d7745d54a0370d02636026
chrisdmitri <chrisdmitri@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2007-04-01 03:32:16 +0000

    Removing unused search forms, and changing the name of the search field to "criteria" which should make searching work again.
    git-svn-id: http://svn.habariproject.org/habari/trunk@634 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/user/themes/k2/searchform.php
+++ htdocs/user/themes/k2/searchform.php
@@ -1,12 +1,4 @@
 <form method="get" id="searchform" action="<?php URL::out('search'); ?>">
-	<input type="text" id="s" name="s" value="search blog archives">
-	<input type="submit" id="searchsubmit" value="go">
-</form>
-<form method="get" id="searchform" action="<?php URL::out('search'); ?>">
-	<input type="text" id="s" name="s" value="search blog archives">
-	<input type="submit" id="searchsubmit" value="go">
-</form>
-<form method="get" id="searchform" action="<?php URL::out('search'); ?>">
-	<input type="text" id="s" name="s" value="search blog archives">
+	<input type="text" id="s" name="criteria" value="search blog archives">
 	<input type="submit" id="searchsubmit" value="go">
 </form>
