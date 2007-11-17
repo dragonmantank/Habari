032b7b6e53f215c4cf66542f7e40d882b2e12c8e
freakerz <freakerz@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2007-11-17 03:32:06 +0000

    Revised the UserHandler::act_login() and login form.
    Fixes issue 449.
    
    git-svn-id: http://svn.habariproject.org/habari/trunk@1077 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- htdocs/user/themes/k2/loginform.php
+++ htdocs/user/themes/k2/loginform.php
@@ -1,12 +1,12 @@
 <!-- loginform -->
 <?php
-if ( isset( $error ) ) {
+if ( isset( $error ) && ( $error == 'Bad credentials' ) ) {
 ?>
      <p>That login is incorrect.</p>
 
 <?php
 }
-if ( isset( $user ) && is_object( $user ) ) {
+if ( isset( $user ) && ( $user instanceOf User ) ) {
 ?>
      <p>You are logged in as <a href="<?php URL::out( 'admin', 'page=user&user=' . $user->username ) ?>" title="Edit Your Profile"><?php echo $user->username; ?></a>.</p>
      <p>Want to <a href="<?php Site::out_url( 'habari' ); ?>/user/logout">log out</a>?</p>
