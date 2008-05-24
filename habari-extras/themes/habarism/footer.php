84f254914fdc73a80f1f8a4e60568863fb591237
caius <caius@653ae4dd-d31e-0410-96ef-6bf7bf53c507>
2008-05-24 07:19:16 +0000

    Fixing the last commit
    Removing the duplicates (~1240 files!) that were mistakenly committed in error.
    
    
    git-svn-id: http://svn.habariproject.org/habari/trunk@1817 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- habari-extras/themes/habarism/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<?php
-// Uncomment this to view your DB profiling info
-// include 'db_profiling.php';
-?>
-</body>
-</html>
--- habari-extras/themes/habarism/tweets.php
+++
@@ -1,7 +0,0 @@
-<div class="block" id="twitter">
-	<h3>Twitter</h3>
-	<ul>
-		<li><em><?php echo htmlspecialchars( $tweet_text ); ?></em></li>
-		<li>Via <a href="http://twitter.com/<?php echo urlencode( Options::get( 'twitter:username' )); ?>">Twitter</a></li>
-	</ul>
-</div>
