dragonmantank/Habari

commit ec1507aa174c513d69ea941af64148e9329907d9

You can now pass the setting 'hideIfSinglePage' = true to Theme::page_selector and it will return an empty string, i.e. no page selector.
Merges #716, original patch by lildude. Changed parameter name and default behavior.


git-svn-id: http://svn.habariproject.org/habari/trunk@3350 653ae4dd-d31e-0410-96ef-6bf7bf53c507

htdocs/system/classes/theme.php
@@ -724,6 +724,10 @@ class Theme extends Pluggable
 		// Create the output variable.
 		$out = '';
 
+		if ( $settings['hideIfSinglePage'] === true ) {
+			return '';
+		}
+		
 		foreach ( $pages as $page ) {
 			$settings['page'] = $page;
 

commit 2c179168837bb5a28d27e1c9e54a779a05594d4e

Fixes the 500 errors caused by r3350, but may not necessarily be the best way to do so. I haven't backtracked to see if there's a reason this value is not set, or if it should be. Also, camelCase?
git-svn-id: http://svn.habariproject.org/habari/trunk@3352 653ae4dd-d31e-0410-96ef-6bf7bf53c507

htdocs/system/classes/theme.php
@@ -724,7 +724,7 @@ class Theme extends Pluggable
 		// Create the output variable.
 		$out = '';
 
-		if ( $settings['hideIfSinglePage'] === true ) {
+		if ( isset( $settings['hideIfSinglePage'] ) &&  $settings['hideIfSinglePage'] === true ) {
 			return '';
 		}
 		